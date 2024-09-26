b32b9909e85dc3ebef363248046945bc09f7fe19
Darklega <collplayer20@gmail.com>
2024-09-26 21:41:20 +0300

    Домащнее Задание

---
+++ DZ.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hey, hey\n";
+    cout << "Bye bye bye\n";
+    cout << "Bye bye\n";
+    cout << " \n";
+    cout << "I'm doing this tonight\n";
+    cout << "You're probably gonna start a fight\n";
+    cout << "I know this can't be right\n";
+    cout << "Hey baby come on\n";
+    cout << "I loved you endlessly\n";
+    cout << "When you weren't there for me\n";
+    cout << "So now it's time to leave and make it alone\n";
+}
