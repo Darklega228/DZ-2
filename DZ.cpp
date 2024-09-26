e849c3df48c6abd8e9c790d460cc59e12f3b6684
Darklega <collplayer20@gmail.com>
2024-09-26 22:02:10 +0300

    Домащнее Задание 2.0

--- DZ.cpp
+++ DZ.cpp
@@ -14,4 +14,20 @@ int main()
     cout << "I loved you endlessly\n";
     cout << "When you weren't there for me\n";
     cout << "So now it's time to leave and make it alone\n";
+    cout << " \n";
+    cout << "I know that I can't take no more\n";
+    cout << "It ain't no lie\n";
+    cout << "I want to see you out that door\n";
+    cout << "Baby bye bye bye\n";
+    cout << " \n";
+    cout << "Don't want to be a fool for you\n";
+    cout << "Just another player in your game for two\n";
+    cout << "You may hate me but it ain't no lie\n";
+    cout << "Bye bye bye\n";
+    cout << "Bye bye\n";
+    cout << "Don't really want to make it tough\n";
+    cout << "I just want to tell you that I've had enough\n";
+    cout << "It might sound crazy but it ain't no lie\n";
+    cout << "Baby bye bye bye\n";
+
 }
